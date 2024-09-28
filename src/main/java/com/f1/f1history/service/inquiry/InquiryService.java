d13a39428978f8436584f40d31369d72cc08dc74
kawaguchiryou <kawaguchiryou@192.168.11.3>
2024-09-28 20:25:37 +0900

    お問い合わせ機能実装

--- src/main/java/com/f1/f1history/service/inquiry/InquiryService.java
+++ src/main/java/com/f1/f1history/service/inquiry/InquiryService.java
@@ -1,9 +1,13 @@
 package com.f1.f1history.service.inquiry;
 
+import java.util.List;
+
 import com.f1.f1history.entity.Inquiry;
 
 public interface InquiryService {
 	
 	void insertInquiry(Inquiry inquiry);
+	
+	List<Inquiry> 
 
 }
