497ea59668c7fa7a27277c512f6eaa6661d31164
kawaguchiryou <144888797+maximumry@users.noreply.github.com>
2024-11-30 19:16:36 +0900

    コミット

---
+++ src/main/java/com/f1/f1history/entity/ChatGPTResponse.java
@@ -0,0 +1,16 @@
+package com.f1.f1history.entity;
+
+import lombok.Data;
+
+@Data
+public class ChatGPTResponse {
+
+	//	private String id;
+	//	private String object;
+	//	private Long created;
+	//	private String model;
+	//	private String systemFingerprint;
+	//	private Choices[] choices;
+	//	private Usage usage;
+
+}
